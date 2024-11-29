39d6ae563fc9ccb0c21c4240d96e41ea948e20e1
RevinaTrisnaAini <revinatrisnaaini_2226240102@mhs.mdp.ac.id>
2024-11-29 17:07:02 +0700

    update jwt

--- my-express-mvc/app.js
+++ my-express-mvc/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 //load mongodb db connection
 require('./app_server/models/db');
+require("./app_server/config/passport");
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -36,25 +38,3 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mahasiswa', mhsRouter);
 app.use('/housing', housingRouter);
-// letakan di bawah use Router
-/*app.use("/housing",(req,res,next)=>{
-  res.header("Access-Control-Allow-Origin","*");
-})
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
