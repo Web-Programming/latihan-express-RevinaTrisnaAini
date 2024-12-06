4f511e88d12cd42a4ed2be982a05fa7ce85c2462
RevinaTrisnaAini <revinatrisnaaini_2226240102@mhs.mdp.ac.id>
2024-12-06 17:02:04 +0700

    angular

--- my-app/src/app/register/register.component.ts
+++ my-app/src/app/register/register.component.ts
@@ -1,30 +1,36 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
-
-@Component({
-  selector: 'app-register',
-  imports: [],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
-})
-export class RegisterComponent {
   registerForm: FormGroup;
+  formError: String ="";
+  
+  //Inject class Router dan service authentication  
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(2)]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
   }
+
+  get name() {
+    return this.registerForm.get('name');
+  }
+  
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
   submitRegister(): void {
     if (this.registerForm.valid) {
       const formData = this.registerForm.value;
 
       console.log('Form submitted', formData);
+      //Panggil method submitRegister()
     } else {
-      console.log('Form is not valid');
+      this.formError = 'All fields are required, please try again';
+      //console.log('Form is not valid');
     }
   }
-}
